5668140b6d907b706c8db7598f664f99a80750c2
Snehit <snehitchats@gmail.com>
2020-12-28 11:39:38 +0530

    password reset pin

---
+++ src/helpers/email.helper.js
@@ -0,0 +1,58 @@
+const nodemailer = require("nodemailer");
+
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.PASSWORD,
+//     },
+// });
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: process.env.E_MAIL,
+        pass: process.env.PASSWORDD
+    }
+});
+
+const send = (info) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let result = await transporter.sendMail(info);
+    
+            console.log("Message sent: %s", result.messageId);
+              // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            
+              // Preview only available when sending through an Ethereal account
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
+              // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou... 
+            resolve(result);
+
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+
+const emailProcessor = (email, pin) => {
+
+    const info = {
+        from: '"TS Company" <bernhard.larson@ethereal.email>', // sender address
+        to: email,  //"bar@example.com, baz@example.com", // list of receivers
+        subject: "Password reset Pin", // Subject line
+        text: "Here is your password reset pin" + "This pin will expire in 1 day", // plain text body
+        html: `<b>Hello </b>
+        Here is your pin
+        <b>${pin}</b>
+        This pin will expire in 1 day
+        <p></p>`, // html body
+    }
+
+    send(info);
+}
+
+module.exports = {
+    emailProcessor,
+}
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -4,7 +4,8 @@ const { insertUser, getUserByEmail, getUserById } = require("../model/user/User.
 const { hashPassword, comparePassword } = require("../helpers/bcrypt.helper");
 const { createAccessJWT, createRefreshJWT } = require("../helpers/jwt.helper");
 const { userAuthorization } = require("../middlewares/authorization.middleware");
-const { setPasswordResetPin } = require("../model/resetPin/ResetPin.model")
+const { setPasswordResetPin } = require("../model/resetPin/ResetPin.model");
+const { emailProcessor } = require("../helpers/email.helper");
 
 router.all("/", (req, res, next) => {
     // res.json({ message: "return from user router" });
@@ -80,14 +81,25 @@ router.post("/login", async (req, res) => {
 //C. server side form validation
     //1. create middleware to validate form data
 
+
+// nodemailer is used for sending the code to email.    
+
 router.post("/reset-password", async (req, res) => {
     const { email } = req.body;
 
     const user = await getUserByEmail(email);
 
     if(user && user._id){
-        const setPin = await setPasswordResetPin(email);
-        res.json(setPin);
+        const setPin = await setPasswordResetPin(email);    //setPin is an object which has {pin, email}.
+        const result = await emailProcessor(email, setPin.pin);
+
+        if(result && result.messageId){
+            return res.json({status: "success", 
+            message: "If the email exist in our database, the password reset pin will be sent shortly"});
+        }
+
+        return res.json({status: "success", 
+            message: "Unable to process your request at the moment. Plz try again later!"});
     }
 
     res.json({status: "error", 
